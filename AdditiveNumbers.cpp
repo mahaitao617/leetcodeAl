1ae80570560a67df7013637eefd751657a7ce56d
Haitao Ma(jimmy) <15828010639@163.com>
2016-03-08 20:42:05 +0800

    Update AdditiveNumbers.cpp

--- AdditiveNumbers.cpp
+++ AdditiveNumbers.cpp
@@ -1,4 +1,4 @@
-/*
+/*  mahaitao
 Additive number is a string whose digits can form additive sequence.
 
 A valid additive sequence should contain at least three numbers. Except for the first two numbers, each subsequent number in the sequence must be the sum of the preceding two.
